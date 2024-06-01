471219b81bc02106dc49bdff321f1a5dd3595c16
Abdu <abdure288@gmail.com>
2024-06-01 10:22:56 +0300

    PCE Entities

---
+++ mechanical/Models/Entities/ProductionCapacity/CapacityEstimationCase.cs
@@ -0,0 +1,35 @@
+﻿using mechanical.Models.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mechanical.Models.Entities.ProductionCapacity
+{
+    public class CapacityEstimationCase
+    {
+          
+     
+     
+
+        public Guid Id { get; set; }        
+        public required string CapacityEstimationNo { get; set; }
+        public required string ApplicantName { get; set; }
+        public string CustomerId { get; set; } = string.Empty;
+        public string CustomerEmail { get; set; } = string.Empty;
+        public DateTime CreationAt { get; set; }
+        public Guid DistrictId { get; set; }
+        public DateTime AssignmentDate { get; set; }
+        public DateTime CompletionDate { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public required Guid CaseOriginatorId { get; set; }
+        public virtual ManufacturingCapacity? ManufacturingCapacity { get; set; }
+        public virtual PlantCapacity? PlantCapacity { get; set; }
+        public virtual District? District { get; set; }
+        public virtual CreateUser? CaseOriginatord { get; set; }        
+        
+
+        //public virtual UploadFile? BussinessLicence { get; set; }
+        //public virtual ICollection<Collateral>? Collaterals { get; set; }
+        //[ForeignKey("BussinessLicence")]
+        //public Guid? BussinessLicenceId { get; set; }
+    }
+}
---
+++ mechanical/Models/Enum/ProductionCapacity/Aerospace.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mechanical.Models.Enum.ProductionCapacity
+{
+    public enum Aerospace
+    {
+        [Display(Name = "Aircraft manufacturing")]
+        Aircraftmanufacturing,
+        [Display(Name = "Spacecraft manufacturing")]
+        Spacecraftmanufacturing,
+        [Display(Name = "Satellite manufacturing")]
+        Satellitemanufacturing,
+        [Display(Name = "Aircraft engine manufacturing")]
+        Aircraftenginemanufacturing,
+        [Display(Name = "Avionics manufacturingg")]
+        Avionicsmanufacturing,
+        [Display(Name = "Missile and defense system manufacturing")]
+        Missileanddefensesystemmanufacturing,
+        [Display(Name = "Drone manufacturing")]
+        Dronemanufacturing,
+        [Display(Name = "Aerospace composite materials manufacturing")]
+        Aerospacecompositematerialsmanufacturing,
+        [Display(Name = " Others, please specify")]
+        Otherspleasespecify
+    }
+}
