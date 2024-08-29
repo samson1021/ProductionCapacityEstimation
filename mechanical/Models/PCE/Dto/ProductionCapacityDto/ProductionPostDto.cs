02d268353d6d7f0dbd731e1ae7c15af3bc6d4ab6
Abdu <abdure288@gmail.com>
2024-08-29 13:59:57 +0300

    New Update on Restimation

--- mechanical/Models/PCE/Dto/ProductionCapacityDto/ProductionPostDto.cs
+++ mechanical/Models/PCE/Dto/ProductionCapacityDto/ProductionPostDto.cs
@@ -46,8 +46,10 @@ namespace mechanical.Models.PCE.Dto.ProductionCapacityDto
 
         public IFormFile? UploadLHC { get; set; }
         public IFormFile? ploadshaderentagreement { get; set; }
-        public IFormFile? Uploadbusinesslicense { get; set; }
-        public IFormFile? Machinespecificationdocumen { get; set; }
+        [Required(ErrorMessage = "Please  upload business license document.")]
+        public IFormFile Uploadbusinesslicense { get; set; }
+        [Required(ErrorMessage = "Please upload machine specification document.")]
+        public IFormFile Machinespecificationdocumen { get; set; }
         public IFormFile? Machineoperationmanual { get; set; }
         public IEnumerable<IFormFile>? OtherDocument { get; set; }
 
