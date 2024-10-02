a49a5761eb87acbab57890fc37afaaf4b7f5634d
Khalid Mohammed <khayo7moh@gmail.com>
2024-10-02 11:39:07 -0300

    Updated Layout, mainsidebar and mainheader

--- mechanical/Controllers/DashboardController.cs
+++ mechanical/Controllers/DashboardController.cs
@@ -46,34 +46,56 @@ namespace mechanical.Controllers
             var latestCase = await _caseService.GetMoLatestCases(base.GetCurrentUserId());
             return View(latestCase);
         }
-
-        //public async Task<IActionResult> RoleBased()
-        //{
-        //    var currentUser = base.GetCurrentUserId();
-        //    var role = await _cbeContext.CreateUsers.Include(res=>res.Role).Where(res=>res.Id == currentUser).FirstOrDefaultAsync();
-        //    if (role.Role.Name == "Relation Manager")
-        //        return RedirectToAction("RM");
-
-        //    else if (role.Role.Name == "Maker Manager")
-        //        return RedirectToAction("MM");
-        //    else if (role.Role.Name == "Maker TeamLeader")
-        //        return RedirectToAction("MM");
-        //    else if (role.Role.Name == "Maker Officer")
-        //        return RedirectToAction("MM");
-        //    else if (role.Role.Name == "Checker Manager")
-        //        return RedirectToAction("MM");
-        //    else if (role.Role.Name == "Checker TeamLeader")
-        //        return RedirectToAction("MM");
-        //    else if (role.Role.Name == "Checker Officer")
-        //        return RedirectToAction("MM");
-        //    else return RedirectToAction("Admin");
-
-        //}
         public async Task<IActionResult> HO()
         {
             var latestCase = await _caseService.GetMmLatestCases(base.GetCurrentUserId());
             return View(latestCase);
         }
-    }
+        public async Task<IActionResult> Admin()
+        {
+            var latestCase = await _caseService.GetMmLatestCases(base.GetCurrentUserId());
+            return RedirectToAction("Index", "UserManagment");
+        }
 
+        public async Task<IActionResult> Index(string Role)
+        {
+            var userId = base.GetCurrentUserId();
+            if (Role == null)  
+            {
+                    Role = (await _cbeContext.CreateUsers.Include(res=>res.Role).Where(res=>res.Id == userId).FirstOrDefaultAsync()).Role.Name;
+            }       
+            
+            if (Role == "Relation Manager")
+                return RedirectToAction("RM");
+            else if (Role == "Maker Manager")
+                return RedirectToAction("MM");
+            else if (Role == "Maker TeamLeader")
+                return RedirectToAction("MTL");
+            else if (Role == "Maker Officer")
+                return RedirectToAction("MO");
+            else if (Role == "Checker Manager")
+                return RedirectToAction("MM");
+            else if (Role == "Checker TeamLeader")
+                return RedirectToAction("MM");
+            else if (Role == "Checker Officer")
+                return RedirectToAction("MM");
+            else return RedirectToAction("Admin");
+
+            // if (role.Role.Name == "Relation Manager")
+            //     return RedirectToAction("RM");
+            // else if (role.Role.Name == "Maker Manager")
+            //     return RedirectToAction("MM");
+            // else if (role.Role.Name == "Maker TeamLeader")
+            //     return RedirectToAction("MTL");
+            // else if (role.Role.Name == "Maker Officer")
+            //     return RedirectToAction("MO");
+            // else if (role.Role.Name == "Checker Manager")
+            //     return RedirectToAction("MM");
+            // else if (role.Role.Name == "Checker TeamLeader")
+            //     return RedirectToAction("MM");
+            // else if (role.Role.Name == "Checker Officer")
+            //     return RedirectToAction("MM");
+            // else return RedirectToAction("Admin");
+        }
+    }
 }
