d26796a9b5bde5443b98ed34e53bfffc2c54eb5c
Abdu <abdure288@gmail.com>
2025-03-03 16:00:53 +0300

    new task comment update

--- mechanical/Models/Dto/TaskManagmentDto/TaskCommentReturnDto.cs
+++ mechanical/Models/Dto/TaskManagmentDto/TaskCommentReturnDto.cs
@@ -9,6 +9,7 @@ namespace mechanical.Models.Dto.TaskManagmentDto
         public Guid TaskId { get; set; }
         public required string Comment { get; set; }
         public DateTime CommentDate { get; set; }
-       
+        public virtual CreateUser? User { get; set; }
+
     }
 }
