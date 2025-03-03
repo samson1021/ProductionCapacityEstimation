729f6ddea09e5f86fcb8a2b43612d0f164d2f815
Melese Ayichlie <119291558+meleayi@users.noreply.github.com>
2025-03-03 15:53:35 +0300

    modification on the task sharing detail

--- Signature management/obj/Debug/net6.0/Signature management.AssemblyInfo.cs
+++ Signature management/obj/Debug/net6.0/Signature management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Signature management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a55bdc9f444407c7ea9f06166fdd837bdefb2c4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2a05788b92193c422d67a9c251be73031cb1d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Signature management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Signature management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
