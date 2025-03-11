bcf0588dccf2933383a3a3a398b59a51273a3b21
Abdu <abdure288@gmail.com>
2025-03-11 15:03:23 +0300

    commit on tS

--- Signature management/obj/Debug/net6.0/Signature management.AssemblyInfo.cs
+++ Signature management/obj/Debug/net6.0/Signature management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Signature management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3bc7889d38fb40594cf13f17b2733fd3c748243")]
 [assembly: System.Reflection.AssemblyProductAttribute("Signature management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Signature management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
