3c05b3efc5bd13b9b971369e530b9d5e92d1a18f
Khalid Mohammed <khayo7moh@gmail.com>
2025-05-11 07:33:23 -0300

    removed FirstPubish and Signature management folders

--- Signature management/Model/Dto/AcknowledgementDto/AcknowledgementReturnDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Signature_management.Model.Dto.AcknowledgementDto
-{
-    public class AcknowledgementReturnDto
-    {
-        public string EmployeeID { get; set; } = string.Empty;
-        public string AcknowledgementLetterBase64String { get; set; } = string.Empty;
-        public string ErrorMessage { get; set; }= string.Empty; 
-    }
-}
