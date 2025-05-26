d6d062545f97fab2a38a7e244ddb2516ffe5912a
Khalid Mohammed <khayo7moh@gmail.com>
2025-05-26 06:49:33 -0300

    removed https scripts

--- mechanical/Services/MailService/MailService.cs
+++ mechanical/Services/MailService/MailService.cs
@@ -11,17 +11,14 @@ namespace mechanical.Services.MailService
             {
                 MailMessage message = new MailMessage();
                 message.From = new MailAddress(mailPostDto.SenderEmail);
-                //message.From = new MailAddress("motherlife075@gmail.com");
                 message.To.Add(mailPostDto.RecipantEmail);
                 message.Subject = mailPostDto.Subject;
                 message.Body = "<h3 style=\"color:purple\">" + "Case Valuation Schedule!: " + "</h3><ul><li style=\"color:black\">" + mailPostDto.Body + "</li></ul>";
                 message.IsBodyHtml = true;
                 SmtpClient client = new SmtpClient("mail.cbe.com.et", 587);
-                //SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
                 client.EnableSsl = true;
                 client.UseDefaultCredentials = false;
                 client.Credentials = new System.Net.NetworkCredential(mailPostDto.SenderEmail, mailPostDto.SenderPassword);
-                //client.Credentials = new System.Net.NetworkCredential("motherlife075@gmail.com", "qgazypzgipyrfnuj");
 
                 await client.SendMailAsync(message);
                 return true;
